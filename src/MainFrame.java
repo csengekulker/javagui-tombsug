3eb973f53a5ae31c40ef63c1baee559330d0c12c
csengekulker <k71608883429@ktch.hu>
2022-04-27 10:35:38 +0200

    added components to panel, set panel size

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,9 +1,13 @@
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
+
+  JPanel mainPanel;
   //class variables
   JLabel aboutLabel;
   JLabel exerciseLabel;
@@ -11,7 +15,7 @@ public class MainFrame extends JFrame {
   JLabel sideLabel;
   JTextField sideField;
   JButton calcButton;
-  
+
   public MainFrame() {
 
     this.setComponents();
@@ -23,15 +27,35 @@ public class MainFrame extends JFrame {
   private void setFrame() {
     this.setTitle("App");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setSize(400, 300);
+    // this.setSize(400, 300);
+    this.pack();
     this.setVisible(true);
+    this.add(mainPanel);
   }
 
   private void setPanels() {
+    this.mainPanel = new JPanel();
 
+    mainPanel.add(this.aboutLabel);
+    mainPanel.add(this.exerciseLabel);
+    mainPanel.add(this.taskLabel);
+    mainPanel.add(this.sideLabel);
+    mainPanel.add(this.sideField);
+    mainPanel.add(this.calcButton);
+
+    mainPanel.setLayout(
+      new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS)
+    );
   }
 
   private void setComponents() {
+    this.aboutLabel = new JLabel("Balogh Csenge, Szoft_I_N, 2022.04.27");
+    this.exerciseLabel = new JLabel("Feladat 0312");
+    this.taskLabel = new JLabel("Rombuszba beírható kör sugara");
+    this.sideLabel = new JLabel("Oldal");
+    this.sideField = new JTextField();
+    this.calcButton = new JButton("Számít");
 
+    // this.exerciseLabel.setFont();
   }
 }
