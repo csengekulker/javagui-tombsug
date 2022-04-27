e99221dc78dc6a0b60547d1fbd07cbfee54982b8
csengekulker <k71608883429@ktch.hu>
2022-04-27 10:02:59 +0200

    created project from template

---
+++ src/MainFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+  public MainFrame() {
+
+    this.setTitle("App");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setSize(400, 300);
+    this.setVisible(true);
+  }
+}
