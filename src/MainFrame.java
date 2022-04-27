d4543ad3d839919ebf7dbc05c04828e9f2c54cd9
csengekulker <k71608883429@ktch.hu>
2022-04-27 10:18:42 +0200

    declared class variables

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,11 +1,37 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
+  //class variables
+  JLabel aboutLabel;
+  JLabel exerciseLabel;
+  JLabel taskLabel;
+  JLabel sideLabel;
+  JTextField sideField;
+  JButton calcButton;
+  
   public MainFrame() {
 
+    this.setComponents();
+    this.setPanels();
+    this.setFrame();
+
+  }
+
+  private void setFrame() {
     this.setTitle("App");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setSize(400, 300);
     this.setVisible(true);
+  }
+
+  private void setPanels() {
+
+  }
+
+  private void setComponents() {
+
   }
 }
